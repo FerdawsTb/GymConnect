af561455d88aa19e3e1de37f0255f660963991e9
Ferdaws Teyeb <ferdawsteyeb@gmail.com>
2025-11-03 10:45:16 +0100

    modifications

--- app/src/main/java/ui/fragments/LoginFragment.java
+++ app/src/main/java/ui/fragments/LoginFragment.java
@@ -2,11 +2,16 @@ package ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.gymconnectapplication.R;
 
@@ -17,7 +22,18 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Relie ce fragment au layout XML
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView noAccount = view.findViewById(R.id.textView8);
+
+        noAccount.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+            navController.navigate(R.id.action_signin_to_signup);
+        });
     }
 }
